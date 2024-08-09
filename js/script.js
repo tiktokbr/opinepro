aac21295d46a2e02d2956624deb9633af5027697
tiktokbr <jp.gomes20002@outlook.com>
2024-08-08 21:47:52 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// // Substitua CLIENT_ID e CLIENT_SECRET pelos valores reais
+// const CLIENT_ID = "38152d56-e781-4835-98f4-d51f43e2e23d";
+// const CLIENT_SECRET = "5dc59720-3dc8-45c3-bd25-e9844d48600a";
+
+// // Concatene CLIENT_ID e CLIENT_SECRET com ":"
+// const concatenated = `${CLIENT_ID}:${CLIENT_SECRET}`;
+
+// // Codifique a concatenação em Base64
+// const base64Encoded = btoa(concatenated);
+
+// console.log("Base64 encoded:", base64Encoded);
